7e0d522848fa6e730242ad7626710f34b44fb8b7
Jesus Cocaño <jesus.cocano@gmail.com>
2022-04-01 00:34:25 -0600

    fix button position

--- src/layout/main/nav-link/index.jsx
+++ src/layout/main/nav-link/index.jsx
@@ -31,24 +31,25 @@ const Navlik = () => {
                 <span> / </span> 
                 <a href="ES">ES</a>    
             </div >
-            <Button class='md:my-0 my-7'></Button>
             <div onClick={()=>setOpen(!open)} class='text-3xl  cursor-pointer md:hidden text-ascBlue'>
-                <ion-icon name='menu-sharp' ></ion-icon>  
-                </div>
-                <ul class={`md:pb-0 pb-12 absolute -top-0 inset-y-0 right-0 md:items-center md:static backdrop-filter backdrop-blur-lg bg-opacity-30 border-b border-gray-200 md:border-0 md:z-auto z-[1] w-3/4 h-full md:w-auto md:pl-0 pl-9 transition-all delay-150 ease-in-out duration-100 ${open ? 'visible':'invisible'}`}>
-                    <li onClick={()=>setOpen(!open)} class='text-3xl absolute right-8 top-6 cursor-pointer md:hidden text-ascBlue'>
-                    <ion-icon name={open ? 'close':'menu-sharp'} ></ion-icon>
-                    </li>
+            <ion-icon name='menu-sharp' ></ion-icon>  
+            </div>
+            <ul class={`md:pb-0 pb-12 absolute -top-0 inset-y-0 right-0 md:items-center md:static backdrop-filter backdrop-blur-lg bg-opacity-30 border-b border-gray-200 md:border-0 md:z-auto z-[1] w-3/4 h-full md:w-auto md:pl-0 pl-9 transition-all delay-150 ease-in-out duration-100 ${open ? 'visible':'invisible'}`}>
+                <li onClick={()=>setOpen(!open)} class='text-3xl absolute right-8 top-6 cursor-pointer md:hidden text-ascBlue'>
+                <ion-icon name={open ? 'close':'menu-sharp'} ></ion-icon>
+                </li>
         
-                    { 
+                { 
                     
-                    Links.map((link)=>(
-                        <li key={link.name} class='md:hidden text-x1 my-7 font-fut pr-7 pt-11 font-semibold text-ascBlue text-right'>
-                            <a href={link.link} class=''>{link.name}</a>
-                        </li>
-                    ))
-                }
-                </ul>
+                Links.map((link)=>(
+                    <li key={link.name} class='md:hidden text-x1 my-7 font-fut pr-7 pt-11 font-semibold text-ascBlue text-right'>
+                        <a href={link.link} class=''>{link.name}</a>
+                    </li>
+                ))
+                
+            }
+            </ul>
+            <Button class='md:my-0 my-7'></Button>
         </div>
     );
   }
