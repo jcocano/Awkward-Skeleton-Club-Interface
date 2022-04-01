b8c66331b6e353e45a6801c1222fb365f5864681
Jesus Cocaño <jesus.cocano@gmail.com>
2022-04-01 00:17:17 -0600

    nav-bar

--- src/layout/main/wallet-data/index.jsx
+++ src/layout/main/wallet-data/index.jsx
@@ -1,11 +1,11 @@
+import React from "react";
 
-
-function WalletButton(){
-
+const Button = () => {
     return (
-        <button>
-            Wallet Conect
+        <button class="hidden md:block bg-ascBlue text-white font-fut-300 py-2 px-2 rounded md:ml-8 hover:bg-ascBlueLight duration-500">
+            CONECT WALLET
         </button>
-    );
-
+    )
 }
+
+export default Button;
--- tailwind.config.js
+++ tailwind.config.js
@@ -7,12 +7,19 @@ module.exports = {
       fontFamily:{
         'fut': ['Futura', 'sans-serif']
       }, 
+      backdropFilter: {
+        'none': 'none',
+        'blur': 'blur(20px)',
+      },
       colors: {
         ascYellow: '#edff41',
         ascBlue: '#253b57',
+        ascBlueLight: '#54618A',
         ascWithe: '#ffffff',
       }
     },
   },
-  plugins: [],
+  plugins: [
+    require('tailwindcss-filters'),
+  ],
 }
